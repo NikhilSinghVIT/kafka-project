1aeda113326c3e55f64310cf00486abcd56fa087
NikhilSinghVIT <nikhil.singh1@walmart.com>
2023-05-29 15:29:36 +0530

    adding approach 1 for pushing message

--- src/main/java/com/kafka/project/kafkaproject/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/project/kafkaproject/controller/LibraryEventsController.java
@@ -1,8 +1,10 @@
 package com.kafka.project.kafkaproject.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafka.project.kafkaproject.domain.LibraryEvent;
+import com.kafka.project.kafkaproject.producer.LibraryEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LibraryEventsController {
 
+    @Autowired
+    private LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         //invoke kafka producer
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
