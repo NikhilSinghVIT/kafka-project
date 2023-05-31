53c8d43df8a174060c7ca5eb9c8f842cd1a6223f
NikhilSinghVIT <nikhil.singh1@walmart.com>
2023-05-31 14:36:41 +0530

    added put library event method

--- src/main/java/com/kafka/project/kafkaproject/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/project/kafkaproject/controller/LibraryEventsController.java
@@ -4,17 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafka.project.kafkaproject.domain.LibraryEvent;
 import com.kafka.project.kafkaproject.domain.LibraryEventType;
 import com.kafka.project.kafkaproject.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@Slf4j
 public class LibraryEventsController {
 
     @Autowired
@@ -26,5 +26,18 @@ public class LibraryEventsController {
         libraryEvent.setLibraryEventType(LibraryEventType.NEW);
         libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> putLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+
+        log.info("LibraryEvent : {} ",libraryEvent );
+        if(libraryEvent.getLibraryEventId()==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibraryEventId");
+        }
+
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+        libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
     }
 }
